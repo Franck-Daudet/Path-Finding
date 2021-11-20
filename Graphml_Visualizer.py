462e454bee5e1b4ce2a12e712672f2f79f737c34
nicolas-jaubert <nicolas.jaubert@etu.univ-amu.fr>
2021-11-20 15:13:28 +0100

    ajout de de l'algo de dijkstra

--- Graphml_Visualizer.py
+++ Graphml_Visualizer.py
@@ -1,9 +1,15 @@
-import osmnx as ox
 import networkx as nx
+import matplotlib.pyplot as plt
+import osmnx as ox
+from custom_dijkstra import *
 
-filename = input("Nom du fichier .graphml : ")
+def main():
+    filename = input("Nom du fichier .graphml : ")
 
-G = ox.load_graphml('graphml/' + filename + '.graphml')
-G = ox.bearing.add_edge_bearings(G, precision=1)
+    G = ox.load_graphml('path-finding/Path-Finding/graphml/' + filename + '.graphml')
+    G = ox.bearing.add_edge_bearings(G, precision=1)
+    
+    ox.plot.plot_graph_route(G, custom_dijkstra(G, 26762546, 26762549), 'r')
 
-ox.plot_graph(G)
+if __name__ == "__main__":
+    main()
